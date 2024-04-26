24ed56a41c1a67ba2d0d1ab776fb815382217b7c
quorafind <951011105@qq.com>
2024-04-26 11:08:22 +0800

    fix: cannot create edge when there is no edge

--- linkNodesInCanvasIndex.ts
+++ linkNodesInCanvasIndex.ts
@@ -10,11 +10,12 @@ import {
 	TFile
 } from 'obsidian';
 import { AllCanvasNodeData, CanvasTextData, NodeSide } from "./canvas";
+import { CanvasEdgeData } from "obsidian/canvas";
 
 export default class LinkNodesInCanvas extends Plugin {
 	async onload() {
-		this.registerCustomCommands()
-		this.registerCustomSuggester()
+		this.registerCustomCommands();
+		this.registerCustomSuggester();
 	}
 
 	registerCustomCommands() {
@@ -32,20 +33,32 @@ export default class LinkNodesInCanvas extends Plugin {
 						const canvas = canvasView.canvas;
 						const selection = canvas.selection;
 						// @ts-ignore
-						const fileNodes = Array.from(selection).filter((node)=> node?.filePath !== undefined);
-						if(fileNodes.length === 0) return;
+						const fileNodes = Array.from(selection).filter((node) => node?.filePath !== undefined);
+						if (fileNodes.length === 0) return;
 
-						const resolvedLinks = app.metadataCache.resolvedLinks;
-						fileNodes.forEach((node)=> {
+						const resolvedLinks = this.app.metadataCache.resolvedLinks;
+						const allEdgesData: CanvasEdgeData[] = [];
+						fileNodes.forEach((node) => {
 							// @ts-ignore
 							const allLinks = (Object.keys(resolvedLinks[node.filePath]) as Array<string>);
-							for(let i = 0; i < fileNodes.length; i++) {
+							for (let i = 0; i < fileNodes.length; i++) {
 								// @ts-ignore
-								if(allLinks.includes(fileNodes[i].filePath)) {
-									if(node !== fileNodes[i]) this.createEdge(node, fileNodes[i], canvas);
+								if (allLinks.includes(fileNodes[i].filePath)) {
+									if (node !== fileNodes[i]) {
+										const newEdge = this.createEdge(node, fileNodes[i], canvas);
+										allEdgesData.push(newEdge);
+									}
 								}
 							}
 						});
+
+						const currentData = canvas.getData();
+						currentData.edges = [
+							...currentData.edges,
+							...allEdgesData,
+						];
+
+						canvas.setData(currentData);
 						canvas.requestSave();
 					}
 
@@ -66,19 +79,18 @@ export default class LinkNodesInCanvas extends Plugin {
 			for (let n = 0; n < e; n++) {
 				t.push((16 * Math.random() | 0).toString(16));
 			}
-			return t.join("")
-		}
+			return t.join("");
+		};
 
-		const edge = canvas.edges.get(canvas.getData().edges.first()?.id);
+		const edgeData: CanvasEdgeData = {
+			id: random(16),
+			fromSide: 'right',
+			fromNode: node1.id,
+			toSide: 'left',
+			toNode: node2.id
+		};
 
-		if(edge) {
-			const tempEdge = new edge.constructor(canvas, random(16), {side: "right", node: node1}, {side: "left", node: node2})
-			canvas.addEdge(tempEdge);
-			tempEdge.attach();
-			tempEdge.render();
-		}else {
-			new Notice("You should have at least one edge in the canvas to use this command.");
-		}
+		return edgeData;
 	}
 
 	createEdgeBasedOnNodes(node1: any, node2: any, canvas: any, side: NodeSide) {
@@ -87,50 +99,72 @@ export default class LinkNodesInCanvas extends Plugin {
 			for (let n = 0; n < e; n++) {
 				t.push((16 * Math.random() | 0).toString(16));
 			}
-			return t.join("")
+			return t.join("");
+		};
+		let tempEdge: any;
+		let fromTargetSide: NodeSide;
+		let toTargetSide: NodeSide;
+
+		switch (side) {
+			case "left":
+				fromTargetSide = "left";
+				toTargetSide = "right";
+				break;
+			case "right":
+				fromTargetSide = "right";
+				toTargetSide = "left";
+				break;
+			case "top":
+				fromTargetSide = "top";
+				toTargetSide = "bottom";
+				break;
+			case "bottom":
+				fromTargetSide = "bottom";
+				toTargetSide = "top";
+				break;
+			case "top-left":
+				fromTargetSide = "top";
+				toTargetSide = "right";
+				break;
+			case "top-right":
+				fromTargetSide = "top";
+				toTargetSide = "left";
+				break;
+			case "bottom-left":
+				fromTargetSide = "bottom";
+				toTargetSide = "right";
+				break;
+			case "bottom-right":
+				fromTargetSide = "bottom";
+				toTargetSide = "left";
+				break;
 		}
 
-		const edge = canvas.edges.get(canvas.getData().edges.first()?.id);
+		tempEdge = {
+			id: random(16),
+			fromSide: fromTargetSide,
+			fromNode: node1.id,
+			toSide: toTargetSide,
+			toNode: node2.id
+		};
 
-		if(edge) {
-			let tempEdge: any;
+		const currentData = canvas.getData();
 
-			console.log(side);
-
-			switch (side) {
-				case "left":
-					tempEdge = new edge.constructor(canvas, random(16), {side: "left", node: node1}, {side: "right", node: node2})
-					break;
-				case "right":
-					tempEdge = new edge.constructor(canvas, random(16), {side: "right", node: node1}, {side: "left", node: node2})
-					break;
-				case "top":
-					tempEdge = new edge.constructor(canvas, random(16), {side: "top", node: node1}, {side: "bottom", node: node2})
-					break;
-				case "bottom":
-					tempEdge = new edge.constructor(canvas, random(16), {side: "bottom", node: node1}, {side: "top", node: node2})
-					break;
-				case "top-left":
-					tempEdge = new edge.constructor(canvas, random(16), {side: "top", node: node1}, {side: "right", node: node2})
-					break;
-				case "top-right":
-					tempEdge = new edge.constructor(canvas, random(16), {side: "top", node: node1}, {side: "left", node: node2})
-					break;
-				case "bottom-left":
-					tempEdge = new edge.constructor(canvas, random(16), {side: "bottom", node: node1}, {side: "right", node: node2})
-					break;
-				case "bottom-right":
-					tempEdge = new edge.constructor(canvas, random(16), {side: "bottom", node: node1}, {side: "left", node: node2})
-					break;
-			}
-
-			canvas.addEdge(tempEdge);
-
-			tempEdge.attach();
-			tempEdge.render();
-		}else {
-			new Notice("You should have at least one edge in the canvas to use this command.");
+		if (currentData.edges.some((edge: CanvasEdgeData) => {
+			return edge.fromNode === tempEdge.fromNode && edge.toNode === tempEdge.toNode;
+		})) {
+			new Notice("Edge already exists between nodes");
+			return;
 		}
+
+		currentData.edges = [
+			...currentData.edges,
+			tempEdge,
+		];
+
+		canvas.setData(currentData);
+		canvas.requestSave();
+
 	}
 
 	onunload() {
@@ -153,6 +187,20 @@ class NodeSuggest extends EditorSuggest<AllCanvasNodeData> {
 	constructor(plugin: LinkNodesInCanvas) {
 		super(plugin.app);
 		this.plugin = plugin;
+
+		this.setInstructions([
+			{
+				command: 'Ctrl/Cmd + Enter',
+				purpose: 'Link to node and generate link',
+			}
+		]);
+
+		this.scope.register(['Mod'], 'Enter', (evt) => {
+			evt.preventDefault();
+
+			// @ts-ignore
+			this.suggestions.useSelectedItem(evt);
+		});
 	}
 
 	getNodes(): AllCanvasNodeData[] {
@@ -207,15 +255,15 @@ class NodeSuggest extends EditorSuggest<AllCanvasNodeData> {
 		const results = this.nodes.filter((node) => {
 			switch (node.type) {
 				case "text":
-					if(node.id === this.original.id || node.text.trim() === "") return false;
+					if (node.id === this.original.id || node.text.trim() === "") return false;
 					return this.fuzzySearch(node.text.toLowerCase());
 				case "file":
 					return this.fuzzySearch(node.file.toLowerCase());
 				case "group":
-					if(node.label?.trim()) return this.fuzzySearch(node.label?.toLowerCase());
+					if (node.label?.trim()) return this.fuzzySearch(node.label?.toLowerCase());
 					else return false;
 				case "link":
-					if(node.url.trim().length === 0) return false;
+					if (node.url.trim().length === 0) return false;
 					return this.fuzzySearch(node.url.toLowerCase());
 			}
 		});
@@ -226,42 +274,48 @@ class NodeSuggest extends EditorSuggest<AllCanvasNodeData> {
 	renderSuggestion(suggestion: AllCanvasNodeData, el: HTMLElement): void {
 		let outer: HTMLElement;
 		let iconEl: HTMLElement;
-		outer = el.createDiv({ cls: "ltn-suggester-container" });
+		outer = el.createDiv({cls: "ltn-suggester-container"});
 		switch (suggestion.type) {
 			case "text":
-				iconEl = outer.createDiv({ cls: "ltn-suggester-icon" });
+				iconEl = outer.createDiv({cls: "ltn-suggester-icon"});
 				setIcon(iconEl, "sticky-note");
-				outer.createDiv({ cls: "ltn-text-node" }).setText(`${suggestion.text}`);
+				outer.createDiv({cls: "ltn-text-node"}).setText(`${suggestion.text}`);
 				break;
 			case "file":
-				iconEl = outer.createDiv({ cls: "ltn-suggester-icon" });
+				iconEl = outer.createDiv({cls: "ltn-suggester-icon"});
 				setIcon(iconEl, "file-text");
-				outer.createDiv({ cls: "ltn-file-node" }).setText(`${suggestion.file}`);
+				outer.createDiv({cls: "ltn-file-node"}).setText(`${suggestion.file}`);
 				break;
 			case "group":
-				iconEl = outer.createDiv({ cls: "ltn-suggester-icon" });
+				iconEl = outer.createDiv({cls: "ltn-suggester-icon"});
 				setIcon(iconEl, "box-select");
-				outer.createDiv({ cls: "ltn-group-node" }).setText(`${suggestion.label}`);
+				outer.createDiv({cls: "ltn-group-node"}).setText(`${suggestion.label}`);
 				break;
 			case "link":
-				iconEl = outer.createDiv({ cls: "ltn-suggester-icon" });
+				iconEl = outer.createDiv({cls: "ltn-suggester-icon"});
 				setIcon(iconEl, "link");
-				outer.createDiv({ cls: "ltn-link-node" }).setText(`${suggestion.url}`);
+				outer.createDiv({cls: "ltn-link-node"}).setText(`${suggestion.url}`);
 				break;
 		}
 
 	}
 
-	selectSuggestion(suggestion: AllCanvasNodeData): void {
+	selectSuggestion(suggestion: AllCanvasNodeData, evt: MouseEvent | KeyboardEvent): void {
 		if (this.context) {
-			(this.context.editor as Editor).replaceRange(
-				``,
+			const editor = (this.context.editor as Editor);
+			const updatedText = (evt.ctrlKey || evt.metaKey) && suggestion.type === 'file' ? `[[${suggestion.file}]]` : '';
+			editor.replaceRange(
+				updatedText,
 				this.context.start,
 				this.end === 0 ? {
 					ch: this.context.end.ch + 2,
 					line: this.context.end.line
 				} : this.context.end
 			);
+			editor.setCursor({
+				line: this.context.end.line,
+				ch: this.context.end.ch + updatedText?.length - 2
+			});
 
 			const targetNode = this.canvas.nodes.get(suggestion.id);
 			const side = this.getDirectionText(this.original.x, this.original.y, targetNode.x, targetNode.y);
